b3f3aa7c756cfdca3e6dd2ddba72eebb4866bd0a
Miroslaw Krok <miroslaw.krok@gmail.com>
2025-03-24 11:26:01 +0100

    wip

--- runner.js
+++ runner.js
@@ -96,7 +96,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \**************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar locOnChange = function locOnChange(data) {\n  window.myPosition = {\n    lat: data.latitude,\n    lng: data.longitude\n  };\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (locOnChange);\n\n//# sourceURL=webpack://app/./src/functions/locOnChange.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar locOnChange = function locOnChange(data) {\n  window.myPosition = {\n    lat: data.latitude,\n    lng: data.longitude\n  };\n  document.querySelector('mk-body').innerHTML += JSON.stringify(data);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (locOnChange);\n\n//# sourceURL=webpack://app/./src/functions/locOnChange.js?");
 
 /***/ }),
 
